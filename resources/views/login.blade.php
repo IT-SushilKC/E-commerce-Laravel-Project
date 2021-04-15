86a0f860b732468924175231ce0a314d7765f6f9
SushilKC <sushilkc2060@gmail.com>
2021-04-15 16:06:07 +0545

    Successful

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,19 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 offset-md-4">
-        <form>
+        <form action="/login" method="post">
+        @csrf
         <div class="form-group">
                 <label for="exampleInputEmail1">Name</label>
-                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
             </div>
             
             <button type="submit" class="btn btn-primary">Submit</button>
